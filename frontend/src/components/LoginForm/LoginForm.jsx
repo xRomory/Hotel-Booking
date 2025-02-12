f1d448cd49bbd7ed71963211e43846c9563b9a28
Chris Lawrence De Vera <xRomory>
2025-02-12 22:50:53 +0800

    Login page revised. Make the background into a slideshow

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './LoginForm.scss'
 import { Link } from 'react-router-dom';
 import { FaHotel } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import { assets } from '../../assets/assets';
+
+const bgImages = [
+  assets.hotelBg1,
+  assets.hotelBg2,
+  assets.hotelBg3,
+  assets.hotelBg4,
+  assets.hotelBg5,
+  assets.hotelBg6,
+  assets.hotelBg7
+];
 
 const LoginForm = () => {
+
+  const [currentBg, setCurrentBg] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBg((prevBg) => (prevBg + 1) % bgImages.length);
+    }, 5000); // Change background every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="login-container">
+    <div
+      className="login-container"
+      style={{
+        background: `linear-gradient(rgba(33, 33, 33, 0.6), rgba(33, 33, 33, 0.6)), url(${bgImages[currentBg]}) no-repeat center`,
+        backgroundSize: "cover",
+        transition: "background-image 1s ease-in-out"
+      }}
+    >
+
       <div className="wrapper flex">
         <div className="logo-div">
           <Link to="/" className="logo">
@@ -29,7 +59,7 @@ const LoginForm = () => {
             <input type="password" placeholder='Password' required/>
             <FaLock className='icon' />
           </div>
-          
+
           <div className="remember-forgot">
             <label>
               <input type="checkbox" /> Remember Me

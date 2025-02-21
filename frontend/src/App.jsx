4c6ea8a5cf2ea1c9b4cc3930d08b2160fe695dcb
Chris Lawrence De Vera <xRomory>
2025-02-21 22:03:00 +0800

    Fixed merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import FoodPage from './pages/Foods/Foods';
-// import HotelRoom from './pages/Rooms/Rooms'
+import RoomPage from './pages/Rooms/Rooms';
+import RoomDetailsPage from './pages/RoomDetails/RoomDetails';
 // import HotelResorts from './components/HotelResorts/HotelResorts';
 // import About from './components/About/About';
-
+import HotelLoginPage from './pages/Login/HotelLoginPage';
+import HotelAmenities from './pages/HotelAmenities/HotelAmenities';
+import HotelRegistration from './pages/Registration/HotelRegistration';
 
 function App() {
   return(
@@ -29,6 +34,10 @@ const MainLayout = () => {
         {/* <Route path='/Rooms' element={} */}
         <Route path="/rooms" element={<RoomPage/>}/>
         <Route path="/Foods" element={<FoodPage/>} />
+        <Route path="/Login" element={<HotelLoginPage/>} />
+        <Route path="/Registration" element={<HotelRegistration/>} />
+        <Route path="/HotelAmenities" element={<HotelAmenities/>} />
+        <Route path ="rooms/:id" element ={<RoomDetailsPage />} />
       </Routes>
       {!hideNavbarFooter && <Footer />}
     </div>
